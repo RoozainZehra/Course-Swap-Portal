8e23197fccb1678de509137723833dc511dbe98a
RoozainZehra <rq08445@st.habib.edu.pk>
2025-04-12 15:55:56 +0500

    Notifications Screen

--- HU-CourseSwap/src/App.jsx
+++ HU-CourseSwap/src/App.jsx
@@ -5,6 +5,7 @@ import ProfilePage from './pages/profile.jsx';
 import Dashboard from './pages/dashboard.jsx';
 import RequestsPage from './pages/requests.jsx';
 import AddRequest from './pages/addRequests.jsx';  // Import AddRequest page
+import NotificationsPage from './pages/Notifications.jsx';
 
 import './App.css';  // Assuming you have global styles
 // import './styles/dashboard.css'; // or wherever your CSS is
@@ -18,6 +19,7 @@ const App = () => {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/my-requests" element={<RequestsPage />} />
         <Route path="/add-request" element={<AddRequest />} />
+        <Route path="/notifications" element={<NotificationsPage />} />        
       </Routes>
     </Router>
   );
