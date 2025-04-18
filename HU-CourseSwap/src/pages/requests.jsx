22bfcab258bbf8c87a8b6590bdff35d70297e962
RoozainZehra <rq08445@st.habib.edu.pk>
2025-04-18 19:06:31 +0500

    added delete functionality

--- HU-CourseSwap/src/pages/requests.jsx
+++ HU-CourseSwap/src/pages/requests.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
-import EmptyState from '../components/EmptyState';
+import UserRequests from '../components/UserRequests';
 import '../styles/requests.css';
 
 const RequestsPage = () => {
   return (
     <div className="requests-layout">
       <Sidebar />
-
       <div className="requests-main">
         <h1 className="section-title">Your Active Requests</h1>
         <div className="requests-content">
-          {/* <div className="empty-state-wrapper">
-            <EmptyState />
-          </div> */}
-          <div className="swap-card-container">
-            <div className="swap-card">
-              <h4>Requested: OOP → DSA</h4>
-              <p>Status: Pending</p>
-              <button className="swap-card-btn1">Delete Request</button>
-            </div>
-          </div>
+          <UserRequests />
         </div>
       </div>
     </div>
